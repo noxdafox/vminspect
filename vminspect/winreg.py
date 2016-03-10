c770715f2eb13327845593251f608a26dd21488d
Matteo Cafasso <noxdafox@gmail.com>
2016-03-10 22:36:15 +0200

    support older version of hivex library

--- vminspect/winreg.py
+++ vminspect/winreg.py
@@ -34,7 +34,25 @@
 import ntpath
 import codecs
 from pebble import process
-from hivex import Hivex, hive_types
+try:
+    from hivex import Hivex, hive_types
+except ImportError:
+    from hivex import Hivex
+
+    class hive_types:
+        REG_NONE = 0
+        REG_SZ = 1
+        REG_EXPAND_SZ = 2
+        REG_BINARY = 3
+        REG_DWORD = 4
+        REG_DWORD_BIG_ENDIAN = 5
+        REG_LINK = 6
+        REG_MULTI_SZ = 7
+        REG_RESOURCE_LIST = 8
+        REG_FULL_RESOURCE_DESCRIPTOR = 9
+        REG_RESOURCE_REQUIREMENTS_LIST = 10
+        REG_QWORD = 11
+
 from tempfile import NamedTemporaryFile
 
 from vminspect.filesystem import FileSystem, posix_path
