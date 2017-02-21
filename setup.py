a5ef0a0b2bc5e35279619470931cef17b348c60d
Matteo Cafasso <noxdafox@gmail.com>
2017-02-21 21:30:03 +0200

    release 0.0.20
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def read(fname):
 
 setup(
     name="vminspect",
-    version="0.0.19",
+    version="0.0.20",
     author="Matteo Cafasso",
     author_email="noxdafox@gmail.com",
     description=("Virtual Machine disk inspection library"),
